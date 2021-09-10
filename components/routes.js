ec83782601e4a67bfe77bc327b1b2b24acbf2fec
Mariano Davancens <mdavancens@exolgan.com>
2021-09-09 21:18:27 -0300

    Reentrega desafío

--- components/routes.js
+++ components/routes.js
@@ -1,21 +1,21 @@
-const productos = [
+let productos = [
 	{
 		"title": "Escuadra",
 		"price": "123.45",
 		"thumbnail": "https://rfmayorista.com.ar/wp-content/uploads/2020/03/REGLA-ECONM_15-CM..jpg",
-		"id": "1"
+		"id": 1
 	},
 	{
 		"title": "Calculadora",
 		"price": "234.56",
 		"thumbnail": "https://www.soscomputacion.com.ar/10024/calculadora-electronica-st39221-12-digitos-grandes-display-grande.jpg",
-		"id": "2"
+		"id": 2
 	},
 	{
 		"title": "Globo Terráqueo",
 		"price": "345.67",
 		"thumbnail": "https://m.media-amazon.com/images/I/91JEXV3kk1L._AC_SL1500_.jpg",
-		"id": "3"
+		"id": 3
 	}
 ];
 
@@ -27,6 +27,8 @@ class rutas {
         this.listar = '/api/productos/listar',
         this.listarPorId = '/api/productos/listar/:id',
         this.guardar = '/api/productos/guardar'
+        this.actualizar = '/api/productos/actualizar/:id'
+        this.borrar = '/api/productos/borrar/:id'
     }
     funcionListar = (req,res) => {
         console.log('request a get recibido!');
@@ -40,8 +42,9 @@ class rutas {
     funcionListarPorId = (req,res) => {
         console.log('request a get recibido!');
         let params = req.params;
-        if(Object.entries(params).length > 0 && params.id >= 1 && params.id <= productos.length){
-            let result = productos.find(x => x.id === params.id)
+        params = parseInt(params.id)
+        if(productos.find(x => x.id === params)){
+            let result = productos.find(x => x.id === params)
             res.json(result);
         }else{
             res.json({error : 'producto no encontrado'})
@@ -57,6 +60,29 @@ class rutas {
         }
         productos.push(productoAAgregar)
         res.json(productoAAgregar);
+    }
+    funcionActualizar = (req,res) => {
+        console.log('request de put recibida')
+        const cuerpo = req.body;
+        const params = parseInt(req.params.id);
+        productos[params -1] = {
+            ...cuerpo,
+            "id" : params
+        }
+        res.json(productos[params - 1]);
+    }
+    
+    funcionBorrar = (req,res) => {
+        console.log('request de delete recibida')
+        let params = parseInt(req.params.id)
+        if(productos.find(x => x.id = params)){
+            let result = productos.find(x => x.id = params)
+            console.log(result)
+            productos.splice(result.id - 1,1);
+            res.json(productos)
+        } else{
+            res.send('No existe el producto');
+        }        
     }
 }
 
